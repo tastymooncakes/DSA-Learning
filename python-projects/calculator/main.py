235b5e68af7ef0ded256694de55cdf316f9b1898
Ethan Wu <ethanwu@Ethans-MacBook-Air.local>
2025-04-10 20:35:58 +0800

    update calculator to use mapping

--- python-projects/calculator/main.py
+++ python-projects/calculator/main.py
@@ -1,28 +1,43 @@
 import argparse
 
+def add(a, b):
+
+    return a + b
+
+def subtract(a, b):
+
+    return a - b
+
+def mulitply(a, b):
+
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        print("Error : Division by zero")
+        return None
+    
+    return a / b
+
 def main():
     parser = argparse.ArgumentParser(description="Simple calculator")
-    parser.add_argument("operation", choices=["+", "-", "x", "/"], help="calculator operation to perform")
+    parser.add_argument("operation", choices=["+", "-", "*", "/"], help="calculator operation to perform")
     parser.add_argument("num1", type=float, help="first number")
     parser.add_argument("num2", type=float, help="second number")
 
     args = parser.parse_args()
-    if args.operation == "+":
-        result = args.num1 + args.num2
-    elif args.operation == "-":
-        result = args.num1 - args.num2
-    elif args.operation == "x":
-        result = args.num1 * args.num2
-    elif args.operation == "/":
-        if args.num2 == 0:
-            print("Error: Division by zero")
-            return
-        result = args.num1 / args.num2
-    else:
-        print("Invalid Operation")
-        return
     
-    print(f"Result: {result}")
+    operations = {
+        "+": add,
+        "-": subtract,
+        "*": mulitply,
+        "/": divide
+    }
+
+    result = operations.get(args.operation)(args.num1, args.num2)
+    
+    if result is not None:
+        print(f"Result: {result}")
 
 if __name__ == "__main__":
     main()
